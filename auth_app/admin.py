ecf56870fb63bd30f4c8e3b171afbd9c4ee8785a
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2026-06-03 14:35:20 +0200

    Register CustomUser in admin with type field

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    """Admin-Konfiguration für CustomUser, erweitert um das 'type'-Feld."""
+
+    # Listenansicht: welche Spalten in der User-Liste angezeigt werden
+    list_display = ('username', 'email', 'type', 'is_staff', 'date_joined')
+
+    # Filter-Sidebar rechts
+    list_filter = UserAdmin.list_filter + ('type',)
+
+    # Detail-/Bearbeitungsansicht eines bestehenden Users
+    fieldsets = UserAdmin.fieldsets + (
+        ('Coderr', {'fields': ('type',)}),
+    )
+
+    # Ansicht beim Anlegen eines neuen Users
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Coderr', {'fields': ('type',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
