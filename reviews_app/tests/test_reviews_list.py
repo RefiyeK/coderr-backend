9861f13677a08af0462ef0206468d0034d0eb771
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2026-06-21 21:11:18 +0200

    test(reviews): add tests for GET endpoint

---
+++ reviews_app/tests/test_reviews_list.py
@@ -0,0 +1,58 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from auth_app.models import CustomUser
+from reviews_app.models import Review
+
+
+class ReviewListTests(APITestCase):
+    """Tests für den Review-Listen-Endpoint (GET /api/reviews/)."""
+
+    def setUp(self):
+        """Erstellt User und zwei Reviews für zwei verschiedene Business-User."""
+        self.customer = CustomUser.objects.create_user(
+            username='customer_user', email='c@coderr.de',
+            password='testpass123', type='customer',
+        )
+        self.business_a = CustomUser.objects.create_user(
+            username='business_a', email='ba@coderr.de',
+            password='testpass123', type='business',
+        )
+        self.business_b = CustomUser.objects.create_user(
+            username='business_b', email='bb@coderr.de',
+            password='testpass123', type='business',
+        )
+        self.review_a = Review.objects.create(
+            business_user=self.business_a,
+            reviewer=self.customer,
+            rating=5,
+            description='Sehr gut',
+        )
+        self.review_b = Review.objects.create(
+            business_user=self.business_b,
+            reviewer=self.customer,
+            rating=3,
+            description='Mittel',
+        )
+        self.url = reverse('review-list')
+
+    def test_authenticated_user_can_list_reviews(self):
+        """Ein authentifizierter User kann die Review-Liste abrufen (200)."""
+        self.client.force_authenticate(user=self.customer)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_unauthenticated_user_cannot_list_reviews(self):
+        """Ein nicht authentifizierter User erhält 401."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_can_be_filtered_by_business_user_id(self):
+        """Filter business_user_id liefert nur Reviews dieses Business-Users."""
+        self.client.force_authenticate(user=self.customer)
+        response = self.client.get(self.url, {'business_user_id': self.business_a.pk})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['business_user'], self.business_a.pk)
